f01ee67ea40347001472286fbb5b3ef816e7189b
moriarty <alexander@dal.ca>
2013-12-09 15:20:01 +0100

    This simple client publishes Env, Problem and Event
    image in mas-group/sdp_ws2013_ompl#22
    mas-group/sdp_ws2013_ompl#36
    
    TODO: 
    
    modify planner node to take this information into ompl and generate a 
    plan.

--- scripts/client.py
+++ scripts/client.py
@@ -8,6 +8,9 @@ roslib.load_manifest(PKG)
 import sys
 import rospy
 
+from sdp_ompl_ros.msg import Bounds
+from sdp_ompl_ros.msg import Bound
+from sdp_ompl_ros.msg import Obstacle
 from sdp_ompl_ros.msg import Path
 from sdp_ompl_ros.msg import PlanningEnvironment
 from sdp_ompl_ros.msg import PlanningProblem
@@ -17,14 +20,62 @@ from ompl import geometric as og
 from ompl import control as oc
 from ompl import app as oa
 
-def planning_client():
-    rospy.wait_for_service('generic_plannin_server')
-    try:
-        generic_planner = rospy.ServiceProxy('generic_plannin_server', GetPlan)
-        plan = generic_planner()
-    except rospy.ServiceException, e:
-        print ""
+from std_msgs.msg import String
+from sdp_ompl_ros.msg import Event
+
+def publish_event(pub, value):
+    event = Event()
+    event.value = value
+    pub.publish(event)
+    rospy.loginfo("Event Published")
+
+def publish_environmnet(pub, values):
+    env = PlanningEnvironment()
+
+    sqr_obstacle = Obstacle()
+    sqr_obstacle.type = "sqaure"
+    sqr_obstacle.size = 3.0
+    sqr_obstacle.pose.x = 0.0
+    sqr_obstacle.pose.y = 0.0
+    sqr_obstacle.pose.theta = 0.0
+    
+    env.bounds.upper.x = 10
+    env.bounds.upper.y = 10
+    env.bounds.lower.x = -10
+    env.bounds.lower.y = -10
+
+    env.obstacles = [sqr_obstacle]
+    pub.publish(env)
+    rospy.loginfo("Environment Published")    
+
+def publish_problem(pub, values):
+    problem = PlanningProblem()
+    problem.start.x = -5.0
+    problem.start.y = -5.0
+    problem.start.theta = 0.0
+    problem.goal.x = 5.0
+    problem.goal.y = 5.0
+    problem.goal.theta = 5.0
+
+    pub.publish(problem)
+    rospy.loginfo("Problem Published")
+
+def main():
+    event_pub = rospy.Publisher('planning_req_event', Event)
+    env_pub = rospy.Publisher('planning_environment', PlanningEnvironment)
+    problem_pub = rospy.Publisher('planning_problem', PlanningProblem)
+
+    while not rospy.is_shutdown():
+        rospy.init_node('planning_client')
+
+        publish_event(event_pub, "start")
+        publish_environmnet(env_pub, "HELLO WORLD")
+        publish_problem(problem_pub, "HELLO WORLD")
+        
+        
+        rospy.sleep(1.0)
+
 
 
 if __name__ == '__main__':
-    planning_client()
+    main()
