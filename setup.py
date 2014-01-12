525e94f7b1093636789806e3efae96b459d873cc
moriarty <alexander@dal.ca>
2014-01-12 12:21:39 +0100

    added ompl_gui and ompl_demo to setup.py
    Also added .gitignore

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
+## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=[''],
+    packages=['ompl_gui', 'ompl_demo'],
     package_dir{'': 'src'},
     requires=['genpy', 'numpy', 'roslib', 'rospkg']
 )
